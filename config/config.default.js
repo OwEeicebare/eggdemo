17f7bba68defae509b49e04530e6a45d15cc05f7
Wang Chao <294993741@qq.com>
2019-02-18 23:01:47 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,23 @@
+exports.keys ='123123124123';
+
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+exports.middleware = [
+  'robot'
+];
+
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ]
+};
